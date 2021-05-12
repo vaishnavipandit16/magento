995493f8f7d2c24b68a5ddc388751f87144ac7f4
vaishnavipandit16 <67839180+vaishnavipandit16@users.noreply.github.com>
2021-05-12 21:47:20 +0530

    deleted old

--- app/code/local/Ccc/Competitor/Block/Adminhtml/Competitor/Attribute/Edit/Tab/Options.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Ccc_Competitor_Block_Adminhtml_Competitor_Attribute_Edit_Tab_Options extends Mage_Eav_Block_Adminhtml_Attribute_Edit_Options_Abstract
-{
-
-	
-}
--- app/code/local/Ccc/Competitor/Model/Resource/Setup.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Ccc_Competitor_Model_Resource_Setup extends Mage_Eav_Model_Entity_Setup{
-    public function getDefaultEntities(){
-        
-    }
-}
-?>
