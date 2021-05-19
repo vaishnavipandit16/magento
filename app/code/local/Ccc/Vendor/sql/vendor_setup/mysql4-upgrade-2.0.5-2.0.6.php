a153c65e3a83bb7ea31ba1fd4c9546e38fcf60ef
vaishnavipandit16 <67839180+vaishnavipandit16@users.noreply.github.com>
2021-05-19 19:55:27 +0530

    deleted

--- app/code/local/Ccc/Vendor/sql/vendor_setup/mysql4-upgrade-2.0.5-2.0.6.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$installer = $this;
-$installer->startSetup();
-$installer->getConnection()->addColumn($installer->getTable('vendor/product'), 'sku','varchar(12) default NULL');
-$installer->endSetup();
-?>
