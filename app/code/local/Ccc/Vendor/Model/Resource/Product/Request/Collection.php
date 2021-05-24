f88d1d02886720819facedfbf53570b5bf5d634f
vaishnavipandit16 <67839180+vaishnavipandit16@users.noreply.github.com>
2021-05-24 12:26:39 +0530

    removed

--- app/code/local/Ccc/Vendor/Model/Resource/Product/Request/Collection.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class Ccc_Vendor_Model_Resource_Product_Request_Collection extends Mage_Eav_Model_Resource_Entity_Attribute_Collection{
-    public function _construct(){
-        $this->_init('vendor/product_request');
-    }
-}
-?>
