73c88c8f1e7ec7e5c4d8e1d1c0057dc54b0a0e6f
vaishnavipandit16 <67839180+vaishnavipandit16@users.noreply.github.com>
2021-05-24 11:59:42 +0530

    deleted

--- app/code/local/Ccc/Vendor/Block/Account/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Ccc_Vendor_Block_Account_Logout extends Mage_Core_Block_Template{
-
-    public function getNewUrl(){
-        $url = $this->helper('vendor')->getLoginUrl();
-        return $url;
-    }
-}
-?>
--- app/code/local/Ccc/Vendor/Block/Marketplace/Product/Group/Form.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Ccc_Vendor_Block_Marketplace_Product_Group_Form extends Mage_Core_Block_Template{
-   public function getSaveUrl(){
-       return $this->getUrl('*/marketplace/savegroup');
-   }
-}
-?>
