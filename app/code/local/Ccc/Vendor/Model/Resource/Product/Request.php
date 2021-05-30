464d6148a82690a6c1df07770fb5f624f9ffc3c7
vaishnavipandit16 <67839180+vaishnavipandit16@users.noreply.github.com>
2021-05-30 15:05:58 +0530

    Changes

--- app/code/local/Ccc/Vendor/Model/Resource/Product/Request.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Ccc_Vendor_Model_Resource_Product_Request extends Mage_Core_Model_Resource_Db_Abstract{
-    protected function _construct(){
-        $this->_init('vendor/product_request','request_id');
-    }
-}
-?>
