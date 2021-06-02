e79ef656ae5390adcff565f91bc56fd769bed7ff
vaishnavipandit16 <67839180+vaishnavipandit16@users.noreply.github.com>
2021-06-02 17:14:07 +0530

    remove old order

--- app/code/local/Ccc/Order/Model/Resource/Order/Cart/Address/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Ccc_Order_Model_Resource_Order_Cart_Address_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract{
-	protected function _construct(){
-		$this->_init('order/order_cart_address');
-	}
-}
-
-?>
